32be7fbd090bd74193d465454decd7b5573a0735
Gianluca Micchi <gianluca.micchi@gmail.com>
2022-12-09 15:42:59 +0000

    Remove writing to disk

--- Tests/test_get_distributions.py
+++ Tests/test_get_distributions.py
@@ -13,13 +13,3 @@ class Test(unittest.TestCase):
         self.assertEqual(len(test_p.profiles_by_key), 9)  # i.e. 9 local key areas
         self.assertEqual(test_p.profiles_by_key[0]['key'], 'Db')  # Starting in Db
         self.assertEqual(test_p.profiles_by_key[-1]['key'], 'Db')  # And ending there too
-
-        # NB: WRITES
-        valid_formats = ['.csv', '.tsv', '.arff', '.json']
-        valid_by_what = ['key', 'chord', 'measure']
-        for vf in valid_formats:
-            for vbw in valid_by_what:
-                for this_bool in [False, True]:
-                    test_p.write_distributions(by_what=vbw,
-                                               write_features=this_bool,
-                                               out_format=vf)
