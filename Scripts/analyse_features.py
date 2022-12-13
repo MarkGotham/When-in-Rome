98fdc438f6405faa5234c2b39914c5a8b02211b6
Gianluca Micchi <gianluca.micchi@gmail.com>
2022-12-13 17:50:49 +0000

    Update scripts

--- Scripts/analyse_features.py
+++ Scripts/analyse_features.py
@@ -1,25 +1,29 @@
-from music21 import converter, roman
+from pathlib import Path
 
+import matplotlib.pyplot as plt
+import pandas as pd
+
+from Code import CORPUS_FOLDER
+from Code.Pitch_profiles.get_distributions import DistributionsFromTabular
 from Code.updates_and_checks import get_corpus_files
-from Code.Pitch_profiles.chord_features import SingleChordFeatures
+
 
 # Check the tests in test_get_distributions and restart from there
 
+def plot_key_over_time(df, name):
+    df.plot.scatter("offset", "key")
+    plt.title(name)
+    plt.show()
+    return
+
+
 if __name__ == '__main__':
-    files = get_corpus_files(file_name="analysis.txt")
-    # for f in files:
-    #     print(f)
-    #     analyses = converter.parse(f, format="romantext")
-    analyses = [converter.parse(f, format="romantext") for f in files]
-    for a in analyses:
-        rns = a.recurse().getElementsByClass(roman.RomanNumeral)
-        for rn in rns:
-            SingleChordFeatures(rn)
+    files = get_corpus_files(file_name="slices_with_analysis.tsv")
+    names = [Path(f).parent.relative_to(CORPUS_FOLDER) for f in files]
 
-    # files = get_corpus_files(file_name="slices_with_analysis.tsv")
+    distributions = {f: DistributionsFromTabular(path_to_tab=f) for f in files}
+    data = [pd.DataFrame(v.data, columns=v.headers).fillna(method="ffill") for k, v in distributions.items()]
+
+    i = 24
+    plot_key_over_time(data[i], names[i])
     print("hi")
-
-
-
-
-# SingleChordFeatures()
--- Scripts/process_corpora.py
+++ Scripts/process_corpora.py
@@ -2,4 +2,4 @@ from Code.updates_and_checks import process_corpus, corpora
 
 if __name__ == '__main__':
     for c in corpora:
-        process_corpus(c, combine=True, slices=True, feedback=True)
+        process_corpus(c, combine=True, slices=True, feedback=True, overwrite=True)
