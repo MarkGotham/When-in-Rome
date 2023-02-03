1d8a65aa6d000a2959ca0fe8da8638c9a0cc235d
Mark Gotham <mrhgotham@gmail.com>
2023-02-03 10:14:07 +0000

    Remaining DCML romantic

--- Code/Resources/metadata.py
+++ Code/Resources/metadata.py
@@ -39,7 +39,6 @@ Some:
 URLs are initialised here with the base and extended when created.
 """
 
-
 # ------------------------------------------------------------------------------
 
 from .. import raw_git
@@ -50,7 +49,7 @@ chorales = dict(
     items=371,
     analysis_source="Bach Chorales",  # NB no public listing, so relative path only
     remote_score_mxl=raw_git + "MarkGotham/Chorale-Corpus/Bach,_Johann_Sebastian/Chorales/"
-    )
+)
 
 madrigals = dict(
     path_within_WiR=["Early_Choral", "Monteverdi,_Claudio"],
@@ -59,16 +58,16 @@ madrigals = dict(
         (3, 20),  # "Madrigals_Book_3"
         (4, 20),
         (5, 8)
-        ),
+    ),
     analysis_source="Monteverdi",
     remote_score_mxl=raw_git + "cuthbertLab/music21/master/music21/corpus/monteverdi/"
-    )
+)
 
 tempered = dict(
     path_within_WiR=["Keyboard_Other", "Bach,_Johann_Sebastian", "The_Well-Tempered_Clavier_I"],
     items=24,
     analysis_source="New"
-    )
+)
 
 chopin_commit = "ad38f0a82e5c50740b1d70a41c84924562bdf9f2"
 
@@ -151,12 +150,12 @@ chopin = dict(
         ((163, None), (67, 4)),
         ((167, None), (67, 2)),
         ((168, None), (68, 4)),
-        ),
+    ),
     analysis_source=raw_git + f"DCMLab/chopin_mazurkas/{chopin_commit}/harmonies/",
     remote_score_mscx=raw_git + f"DCMLab/chopin_mazurkas/{chopin_commit}/MS3/",
     analysis_DT_source="Chopin",  # NB no public listing, so relative path only
     remote_score_krn=raw_git + "craigsapp/chopin-mazurkas/master/kern/",
-    )
+)
 
 lvb_commit = "f131d6dac8605ae7be9f874637330f0b73d11e2f"
 
@@ -227,3 +226,194 @@ sonatas_Mozart = dict(
     # remote_score_krn=raw_git + "craigsapp/mozart-piano-sonatas/master/kern/",
     # Omit ^ Split files too complex (e.g., "sonata06-3m.krn").
 )
+
+debussy_commit = "691842d8bc8e03fbf0453542d86f3d1eb7cf44eb"
+
+debussy_suite_bergamasque = dict(
+    repo_name="debussy_suite_bergamasque",
+    path_within_WiR=["Keyboard_Other", "Debussy,_Claude", "Suite_Bergamasque,_L.75"],
+    item_keys=("Lesure Number", "DCML file Name"),
+    items=(
+        (75, "Prelude"),
+        (75, "Menuet"),
+        (75, "Clair"),
+        (75, "Passepied")
+    ),
+    analysis_source=raw_git + f"DCMLab/debussy_suite_bergamasque/{debussy_commit}/harmonies/",
+    remote_score_mscx=raw_git + f"DCMLab/debussy_suite_bergamasque/{debussy_commit}/MS3/",
+)
+
+dvorak_commit = "13502ce2304525eec6b81c68decc6d7dd48dd1e5"
+
+dvorak_silhouettes = dict(
+    repo_name="dvorak_silhouettes",
+    path_within_WiR=["Keyboard_Other", "Dvořák,_Antonín", "Silhouettes,_Op.8"],
+    item_keys="Number",
+    items=12,
+    analysis_source=raw_git + f"DCMLab/dvorak_silhouettes/{dvorak_commit}/harmonies/",
+    remote_score_mscx=raw_git + f"DCMLab/dvorak_silhouettes/{dvorak_commit}/MS3/",
+)
+
+grieg_commit = "3ae328ea75dd8b2dc360e2be1b9125c604d9e048"
+
+grieg_lyric_pieces = dict(
+    repo_name="grieg_lyric_pieces",
+    path_within_WiR=["Keyboard_Other", "Grieg,_Edvard", "Lyric_Pieces"],
+    item_keys=("Opus", "Number"),
+    items=(
+        (12, 1),
+        (12, 2),
+        (12, 3),
+        (12, 4),
+        (12, 5),
+        (12, 6),
+        (12, 7),
+        (12, 8),
+        (38, 1),
+        (38, 2),
+        (38, 3),
+        (38, 4),
+        (38, 5),
+        (38, 6),
+        (38, 7),
+        (38, 8),
+        (43, 1),
+        (43, 2),
+        (43, 3),
+        (43, 4),
+        (43, 5),
+        (43, 6),
+        (47, 1),
+        (47, 2),
+        (47, 3),
+        (47, 4),
+        (47, 5),
+        (47, 6),
+        (47, 7),
+        (54, 1),
+        (54, 2),
+        (54, 3),
+        (54, 4),
+        (54, 5),
+        (54, 6),
+        (57, 1),
+        (57, 2),
+        (57, 3),
+        (57, 4),
+        (57, 5),
+        (57, 6),
+        (62, 1),
+        (62, 2),
+        (62, 3),
+        (62, 4),
+        (62, 5),
+        (62, 6),
+        (65, 1),
+        (65, 2),
+        (65, 3),
+        (65, 4),
+        (65, 5),
+        (65, 6),
+        (68, 1),
+        (68, 2),
+        (68, 3),
+        (68, 4),
+        (68, 5),
+        (68, 6),
+        (71, 1),
+        (71, 2),
+        (71, 3),
+        (71, 4),
+        (71, 5),
+        (71, 6),
+        (71, 7),
+    ),
+    analysis_source=raw_git + f"DCMLab/grieg_lyric_pieces/{grieg_commit}/harmonies/",
+    remote_score_mscx=raw_git + f"DCMLab/grieg_lyric_pieces/{grieg_commit}/MS3/",
+)
+
+liszt_commit = "fde6d94aca05460603a970f0f7d48ee8b8722854"
+
+liszt_pelerinage = dict(
+    repo_name="liszt_pelerinage",
+    path_within_WiR=["Keyboard_Other", "Liszt,_Franz", "Années_de_pèlerinage"],
+    item_keys=("Searle", "Number", "Name"),
+    items=(
+        (160, 1, "Chapelle_de_Guillaume_Tell"),
+        (160, 2, "Au_Lac_de_Wallenstadt"),
+        (160, 3, "Pastorale"),
+        (160, 4, "Au_Bord_dUne_Source"),
+        (160, 5, "Orage"),
+        (160, 6, "Vallee_dObermann"),
+        (160, 7, "Eglogue"),
+        (160, 8, "Le_Mal_du_Pays_(Heimweh)"),
+        (160, 9, "Les_Cloches_de_Geneve_(Nocturne)"),
+        (161, 1, "Sposalizio"),
+        (161, 2, "Il_Pensieroso"),
+        (161, 3, "Canzonetta_del_Salvator_Rosa"),
+        (161, 4, "Sonetto_47_del_Petrarca"),
+        (161, 5, "Sonetto_104_del_Petrarca"),
+        (161, 6, "Sonetto_123_del_Petrarca"),
+        (161, 7, "Apres_une_lecture_du_Dante"),
+        (162, 1, "Gondoliera"),
+        (162, 2, "Canzone"),
+        (162, 3, "Tarantella_da_Guillaume_Louis_Cottrau._Presto_e_canzone_napolitana"),
+    ),
+    analysis_source=raw_git + f"DCMLab/liszt_pelerinage/{liszt_commit}/harmonies/",
+    remote_score_mscx=raw_git + f"DCMLab/liszt_pelerinage/{liszt_commit}/MS3/",
+)
+
+medtner_commit = "9d2b1991e2c7e3a06da1c41e8bc1dba9fedca05e"
+
+medtner_tales = dict(
+    repo_name="medtner_tales",
+    path_within_WiR=["Keyboard_Other", "Medtner,_Nikolai", "Tales"],
+    item_keys=("Opus", "Number"),
+    items=(
+        (8, 1),
+        (14, 1),
+        (26, 1),
+        (26, 2),
+        (26, 3),
+        (26, 4),
+        (34, 1),
+        (34, 2),
+        (34, 3),
+        (34, 4),
+        (35, 1),
+        (35, 2),
+        (35, 3),
+        (35, 4),
+        (42, 1),
+        (42, 2),
+        (42, 3),
+        (48, 1),
+        (48, 2)
+    ),
+    analysis_source=raw_git + f"DCMLab/medtner_tales/{medtner_commit}/harmonies/",
+    remote_score_mscx=raw_git + f"DCMLab/medtner_tales/{medtner_commit}/MS3/",
+)
+
+schumann_commit = "22cde8be633af1ad6a2c6e08068924cbcd846274"
+
+schumann_kinderszenen = dict(
+    repo_name="schumann_kinderszenen",
+    path_within_WiR=["Keyboard_Other", "Schumann,_Robert", "Kinderszenen,_Op.15"],
+    item_keys="Number",
+    items=13,
+    analysis_source=raw_git + f"DCMLab/schumann_kinderszenen/{schumann_commit}/harmonies/",
+    remote_score_mscx=raw_git + f"DCMLab/schumann_kinderszenen/{schumann_commit}/MS3/",
+)
+
+tchaikovsky_commit = "8794c63afae7cd3a7f3299c57bd326ae5b681b65"
+
+tchaikovsky_seasons = dict(
+    repo_name="schumann_kinderszenen",
+    path_within_WiR=["Keyboard_Other", "Tchaikovsky,_Pyotr", "Seasons,_Op.37a"],
+    item_keys="Number",
+    items=12,
+    analysis_source=raw_git + f"DCMLab/tchaikovsky_seasons/{tchaikovsky_commit}/harmonies/",
+    remote_score_mscx=raw_git + f"DCMLab/tchaikovsky_seasons/{tchaikovsky_commit}/MS3/",
+)
+
+
