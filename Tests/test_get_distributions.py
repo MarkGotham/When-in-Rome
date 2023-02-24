eb92c767f4e379053bc6ecc44f40c7bb9fd6f4cb
Mark Gotham <mrhgotham@gmail.com>
2023-02-24 21:16:43 +0100

    path

--- Tests/test_get_distributions.py
+++ Tests/test_get_distributions.py
@@ -8,7 +8,7 @@ class Test(unittest.TestCase):
 
     def test_two_songs(self):
         base_path = TEST_RESOURCES_FOLDER / "Example"
-        test_p = DistributionsFromTabular(path_to_tab=str(base_path / 'slices_with_analysis.tsv'))
+        test_p = DistributionsFromTabular(path_to_tab=base_path / 'slices_with_analysis.tsv')
         self.assertEqual(len(test_p.profiles_by_key), 9)  # i.e. 9 local key areas
         self.assertEqual(test_p.profiles_by_key[0]['key'], 'Db')  # Starting in Db
         self.assertEqual(test_p.profiles_by_key[-1]['key'], 'Db')  # And ending there too
