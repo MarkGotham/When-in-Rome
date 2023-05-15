01e3c2a0a06ad9c32d70067e9e0066106e41ec46
Gianluca Micchi <gianluca.micchi@gmail.com>
2023-05-15 14:18:38 +0100

    Introduce tests

---
+++ Tests/test_converters_local.py
@@ -0,0 +1,29 @@
+from tempfile import NamedTemporaryFile
+
+from Tests import TEST_RESOURCES_FOLDER
+from Code.converters_local import ConverterRn2Tab, ConverterRn2Dez
+
+fld = TEST_RESOURCES_FOLDER / "Example"
+score_path = fld / "score.mxl"
+tab_path = fld / "analysis_BPS_format.csv"
+dez_path = fld / "analysis_dez_format.dez"
+rn_path = fld / "analysis.txt"
+
+
+def test_rn2tab():
+    c = ConverterRn2Tab()
+    with open(tab_path, "r") as f:
+        ref = f.readlines()
+    with NamedTemporaryFile() as f:
+        c.convert_file(score_path, rn_path, f.name)
+        pred = [x.decode().replace("\r", "") for x in f.readlines()]
+    assert ref == pred
+
+
+def test_rn2dez():
+    c = ConverterRn2Dez()
+    ref = c._load_dez(dez_path)
+    with NamedTemporaryFile() as f:
+        c.convert_file(score_path, rn_path, f.name)
+        pred = c._load_dez(f.name)
+    assert ref == pred
